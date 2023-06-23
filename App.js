d9e73f5189dbf94bcceb623738c4908ad8cf18ac
Shorya goyal <shoryagoyal18@gmail.com>
2023-06-23 22:15:58 +0530

    Footer content added

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <React.Fragment>
+      <Header />
+      <h1>Home</h1>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
